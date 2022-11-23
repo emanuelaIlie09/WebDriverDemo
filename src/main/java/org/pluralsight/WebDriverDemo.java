0862b89454d8288bef48e5b8c35001c327fd3fce
Ema <emanuela.ilie99@e-uvt.ro>
2022-11-23 10:26:27 +0200

    Using the Explicit Wait

--- src/main/java/org/pluralsight/WebDriverDemo.java
+++ src/main/java/org/pluralsight/WebDriverDemo.java
@@ -5,6 +5,10 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class WebDriverDemo {
     public static void main(String[] args) {
@@ -18,6 +22,9 @@ public class WebDriverDemo {
         searchField.click();
         searchField.sendKeys("pluralsight");
         searchField.sendKeys(Keys.ENTER);
+
+        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Imagini")));
 
 
         WebElement imagesLink= driver.findElements(By.linkText("Imagini")).get(0);
